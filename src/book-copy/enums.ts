1085e615bbbf3bb8904b2fac7fc0fd3b967acd4e
gambitier <akash.jadhav.cse@gmail.com>
2022-09-04 23:28:02 +0530

    create bookcopy module

---
+++ src/book-copy/enums.ts
@@ -0,0 +1,21 @@
+export enum BookStatus {
+  AVAILABLE,
+  RESERVED,
+  LOANED,
+  LOST,
+}
+
+export enum ReservationStatus {
+  WAITING,
+  PENDING,
+  CANCELED,
+  NONE,
+}
+
+export enum AccountStatus {
+  ACTIVE,
+  CLOSED,
+  CANCELED,
+  BLACKLISTED,
+  NONE,
+}
