7d0b777f3a74fed805d365cb15fb6c889c8ffa7c
gambitier <akash.jadhav.cse@gmail.com>
2022-09-05 22:11:24 +0530

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,9 +13,19 @@ import { mailerModuleConfigs, twilioModuleConfig } from 'src/configs';
 import { AllExceptionsFilter } from 'src/filters/all-exceptions.filter';
 import { APIResponseInterceptor } from 'src/interceptors/api.response.interceptor';
 import { PrismaService } from 'src/prisma.service';
+import { AddressModule } from './modules/address/address.module';
+import { AuthorModule } from './modules/author/author.module';
+import { BookModule } from './modules/book/book.module';
+import { LibraryModule } from './modules/library/library.module';
+import { PublisherModule } from './modules/publisher/publisher.module';
 
 @Module({
   imports: [
+    PublisherModule,
+    AuthorModule,
+    AddressModule,
+    BookModule,
+    LibraryModule,
     // LoggerModule.forRoot(),
     LoggerModule.forRootAsync({
       imports: [ConfigModule],
